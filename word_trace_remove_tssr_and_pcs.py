8688b596adab985228e42adc55099acb5532f230
shamexln <shamexln@hotmail.com>
2023-12-19 20:40:15 +0800

    only remove tssr

--- word_trace_remove_tssr_and_pcs.py
+++ word_trace_remove_tssr_and_pcs.py
@@ -1,7 +1,7 @@
 import docx
 from sortedcontainers import SortedSet
 
-doc = docx.Document('CH3.5.5.6 可追溯分析报告_ORG.docx')
+doc = docx.Document('CH3.5.5.6 可追溯分析报告.docx')
 arr = []
 tsr = []
 emptyline = 0
@@ -21,30 +21,22 @@ for t in doc.tables:
                                 endIndex = paragraph.text.find('（')
                             if endIndex != -1:
                                 arr.append(paragraph.text[:endIndex])
-                                # print (paragraph.text[:endIndex])
+                                print (paragraph.text[:endIndex])
                         elif paragraph.text.startswith('PCS') :
-                            endIndex = paragraph.text.find('(')
-                            if endIndex == -1:
-                                endIndex = paragraph.text.find('（')
-                            if endIndex != -1:
-                                arr.append(paragraph.text[:endIndex])
-                                # print (paragraph.text[:endIndex])
+                            found = 1
                     str = ''
                     for tt in arr:
                         str = str + tt + '\n'
                     if len(arr) > 0:
                         cell.text = str
-                        print ('######################')
-                        print (cell.text)
-                        print ('######################')
                         arr.clear()
-                    elif len(arr) == 0:
-                        # emptyline = 1
-                        emptyline = 0
+                    elif len(arr) == 0 and found == 1:
+                        emptyline = 1
+                        found = 0
                 i = i + 1
             if emptyline == 1:
                 row._element.getparent().remove(row._element)
                 emptyline = 0
             # if j == 70:
             #    break;  
-doc.save('CH3.5.5.6 可追溯分析报告_remove_tssr.docx')               
+doc.save('Traceability_SRS1_MDR_ZH1.docx')               
